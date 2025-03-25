0070c27837587d14861ba8f6e067aea5f3bc6038
Avanish patidar <104437145+Avanishpatidar@users.noreply.github.com>
2025-03-26 00:45:31 +0530

    Update blog-list.js

--- blog/blog-list.js
+++ blog/blog-list.js
@@ -6,5 +6,10 @@ const blogPosts = [
         date: "12 mar, 2025",
         summary: "What if When Your Fridge Gets Smarter Than You"
     },
-  
+    {
+        title: "AI Hype or Signk?",
+        slug: "ai-hype-signk",
+        date: "25 mar, 2025",
+        summary: "When AI’s big talk might sink us—dot-com vibes, anyone?"
+    }
 ];
